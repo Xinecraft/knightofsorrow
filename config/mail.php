caecf266665652df5bc11502c4bc89b40c675648
Zishan Ansari <kinnngg786@gmail.com>
2016-06-05 03:50:24 +0530

    Fix mail system

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'no-reply@knightofsorrow.tk', 'name' => 'KNIGHTofSORROW'],
 
     /*
     |--------------------------------------------------------------------------
