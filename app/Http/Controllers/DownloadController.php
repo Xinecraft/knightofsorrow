0bf4652c6a5f6fbdb123a987ee60876f5b9f85a7
Zishan Ansari <kinnngg786@gmail.com>
2016-07-11 18:18:11 +0530

    Fix player-detail link and Images of live Player

---
+++ app/Http/Controllers/DownloadController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use File;
+use Response;
+
+class DownloadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function download($name)
+    {
+        switch($name)
+        {
+            case 1:
+                $name = "AMMod.u";
+                break;
+            case 2:
+                $name = "antics_v1.u";
+                break;
+            default:
+                $name = "";
+        }
+
+
+        $file = storage_path() . DIRECTORY_SEPARATOR. "{$name}";
+        //dd($file);
+        if(File::exists($file))
+        {
+            return Response::download($file, $name);
+        }
+        else
+        {
+            return redirect()->home()->with('error','Error! File Not Found');
+        }
+    }
+}
